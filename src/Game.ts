2c3c70d6ed54dd70bbee451e7d92a73db1851297
Masoud Azizpour <masoud.work@gmail.com>
2022-01-20 12:06:21 +0000

    Refactor sameSymbolInRow

--- src/Game.ts
+++ src/Game.ts
@@ -1,96 +1,91 @@
 export enum Symbol {
-    empty = ' ',
-    X = 'X',
-    O = 'O',
+  empty = " ",
+  X = "X",
+  O = "O",
 }
 
-type Coordinate = 0 | 1 | 2
+type Coordinate = 0 | 1 | 2;
 
 interface Position {
-    x: Coordinate ;
-    y: Coordinate ;
+  x: Coordinate;
+  y: Coordinate;
 }
+
 interface Tile {
-    position: Position;
-    symbol: Symbol;
+  position: Position;
+  symbol: Symbol;
 }
 
 export class Game {
-    private _lastSymbol: string = Symbol.empty;
-    private _board: Board = new Board();
+  private _lastSymbol: string = Symbol.empty;
+  private _board: Board = new Board();
 
-    public Play(symbol: Symbol, x: Coordinate, y: Coordinate) : void {
-        //if first move
-        if (this._lastSymbol === Symbol.empty) {
-            //if player is X
-            if (symbol === Symbol.O) {
-                throw new Error("Invalid first player");
-            }
-        }
-        //if not first move but player repeated
-        else if (symbol === this._lastSymbol) {
-            throw new Error("Invalid next player");
-        }
-        //if not first move but play on an already played tile
-        else if (this._board.TileAt({ x, y }).symbol !== Symbol.empty) {
-            throw new Error("Invalid position");
-        }
-
-        // update game state
-        this._lastSymbol = symbol;
-        this._board.AddTileAt(symbol, { x, y });
+  public Play(symbol: Symbol, x: Coordinate, y: Coordinate): void {
+    //if first move
+    if (this._lastSymbol === Symbol.empty) {
+      //if player is X
+      if (symbol === Symbol.O) {
+        throw new Error("Invalid first player");
+      }
+    }
+    //if not first move but player repeated
+    else if (symbol === this._lastSymbol) {
+      throw new Error("Invalid next player");
+    }
+    //if not first move but play on an already played tile
+    else if (this._board.TileAt({ x, y }).symbol !== Symbol.empty) {
+      throw new Error("Invalid position");
     }
 
-    public Winner() : string {
-        if (this._board.RowIsFilled(0) && this._board.SameSymbolInRow(0)) {
-                return this._board.TileAt({ x: 0, y: 0 }).symbol
-        }
+    // update game state
+    this._lastSymbol = symbol;
+    this._board.AddTileAt(symbol, { x, y });
+  }
 
-        if (this._board.RowIsFilled(1) && this._board.SameSymbolInRow(1)) {
-            return this._board.TileAt({ x: 1, y: 0 }).symbol
-        }
-
-        if (this._board.RowIsFilled(2) && this._board.SameSymbolInRow(2)) {
-            return this._board.TileAt({ x: 2, y: 0 }).symbol
-        }
-
-        return Symbol.empty;
+  public Winner(): string {
+    for (let c = 0; c < 3; c++) {
+      const symbol = this._board.SameSymbolInRow(c as Coordinate);
+      if (symbol != Symbol.empty) {
+        return symbol;
+      }
     }
+
+    return Symbol.empty;
+  }
 }
 
-class Board
-{
-    private _plays : Tile[] = [];
+class Board {
+  private _plays: Tile[] = [];
 
-    constructor()
-    {
-        for (let i = 0; i < 3; i++)
-        {
-            for (let j = 0; j < 3; j++)
-            {
-                const tile : Tile = { position: { x: i as Coordinate, y: j as Coordinate }, symbol: Symbol.empty};
-                this._plays.push(tile);
-            }
-        }
+  constructor() {
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        const tile: Tile = {
+          position: { x: i as Coordinate, y: j as Coordinate },
+          symbol: Symbol.empty,
+        };
+        this._plays.push(tile);
+      }
     }
+  }
 
-    public TileAt({ x, y }: Position): Tile {
-        return this._plays.find((t:Tile) => t.position.x === x && t.position.y === y)!
-    }
+  public TileAt(position: Position): Tile {
+    return this._plays.find(
+      (t: Tile) => t.position.x === position.x && t.position.y === position.y
+    )!;
+  }
 
-    public AddTileAt(symbol: Symbol, position: Position) : void
-    {
-        this.TileAt(position).symbol = symbol;
-    }
+  public AddTileAt(symbol: Symbol, position: Position): void {
+    this.TileAt(position).symbol = symbol;
+  }
 
-    public RowIsFilled(row: Coordinate): boolean {
-        return this.TileAt({ x: row, y: 0 })!.symbol !== Symbol.empty &&
-          this.TileAt({ x: row, y: 1 })!.symbol !== Symbol.empty &&
-          this.TileAt({ x: row, y: 2 })!.symbol !== Symbol.empty
-    }
+  public SameSymbolInRow(row: Coordinate): Symbol {
+    const symbol = this.TileAt({ x: row, y: 0 }).symbol;
 
-    public SameSymbolInRow(row: Coordinate): boolean {
-        return this.TileAt({ x: row, y: 0 })!.symbol === this.TileAt({ x: row, y: 1 })!.symbol &&
-          this.TileAt({ x: row, y: 2 })!.symbol === this.TileAt({ x: row, y: 1 })!.symbol
-    }
+    const sameSymbolInRow =
+      symbol === this.TileAt({ x: row, y: 1 }).symbol &&
+      symbol === this.TileAt({ x: row, y: 2 }).symbol;
+
+    return sameSymbolInRow ? symbol : Symbol.empty;
+  }
 }
